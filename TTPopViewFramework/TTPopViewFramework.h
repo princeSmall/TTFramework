bb6e01c69c6290d494af4478407911183c20b25a
le tong <le.tong@ly.com>
2019-11-22 10:49:09 +0800

    first commit

---
+++ TTPopViewFramework/TTPopViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  TTPopViewFramework.h
+//  TTPopViewFramework
+//
+//  Created by le tong on 2019/11/21.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TTPopViewFramework.
+FOUNDATION_EXPORT double TTPopViewFrameworkVersionNumber;
+
+//! Project version string for TTPopViewFramework.
+FOUNDATION_EXPORT const unsigned char TTPopViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TTPopViewFramework/PublicHeader.h>
+
+
